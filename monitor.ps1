580e4e1ab89198d63cf09333ab871c2c40f8722f
vidhena <vidhena16@gmail.com>
2026-07-16 12:10:16 +0530

    4

--- monitor.ps1
+++ monitor.ps1
@@ -1,10 +1,31 @@
-$container = "flask-monitor"
+$ContainerName = "flask-monitor"
 
-$status = docker inspect -f "{{.State.Running}}" $container 2>$null
+$LogFolder = ".\logs"
+$LogFile = "$LogFolder\incidents.log"
 
-if ($status -ne "true") {
-    Write-Host "Container stopped. Restarting..."
-    docker restart $container
-} else {
-    Write-Host "Container is running."
+if (!(Test-Path $LogFolder)) {
+    New-Item -ItemType Directory -Path $LogFolder | Out-Null
+}
+
+$status = docker inspect -f "{{.State.Running}}" $ContainerName
+
+if ($status -eq "true") {
+
+    $message = "$(Get-Date) - Container is RUNNING"
+    Write-Host $message
+    Add-Content -Path $LogFile -Value $message
+
+}
+else {
+
+    $message = "$(Get-Date) - Container stopped. Restarting..."
+    Write-Host $message
+    Add-Content -Path $LogFile -Value $message
+
+    docker restart $ContainerName
+
+    $message = "$(Get-Date) - Container restarted successfully."
+    Write-Host $message
+    Add-Content -Path $LogFile -Value $message
+
 }
